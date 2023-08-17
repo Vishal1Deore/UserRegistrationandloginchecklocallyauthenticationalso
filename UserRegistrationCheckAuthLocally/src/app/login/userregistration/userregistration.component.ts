0bc889cd8b106497e0166db45fd7aca6bd8dc3e0
Vishal1Deore <vishaldeore1997@gmail.com>
2023-08-17 23:54:07 +0530

    successfully logout

--- UserRegistrationCheckAuthLocally/src/app/login/userregistration/userregistration.component.ts
+++ UserRegistrationCheckAuthLocally/src/app/login/userregistration/userregistration.component.ts
@@ -12,25 +12,25 @@ export class UserregistrationComponent {
   constructor(public service:RegserviceService){
 
   }
-  regserviceService:User= new User();
-  registerUserData:User=new User();
+  RegserviceService:User= new User();
+  //RegserviceService:User=new User();
 
   registerUser(){
-    if(this.registerUserData.firstName==undefined||this.registerUserData.lastName==undefined||this.registerUserData.email==undefined||this.registerUserData.address==undefined||this.registerUserData.setPass==undefined)
+    if(this.RegserviceService.firstName==null||this.RegserviceService.lastName==undefined)
     {
       window.alert("all feild are mindatory");
     }
     else{
-      if(this.service.isUserAlreadyPresentInService(this.regserviceService.firstName)==true){
-        window.alert("already user register");
-      }
-      else{
-        this.service.setUserInService(this.regserviceService);
-        window.alert("Registration Successful");
-        this.regserviceService= new User();
-      }
-      
+      if(this.service.isUserAlreadyPresentInService(this.RegserviceService.firstName)==true){
+      window.alert("already user register");
     }
+    else{
+      this.service.setUserInService(this.RegserviceService);
+      window.alert("Registration Successful");
+      this.RegserviceService= new User();
+    }
+    
+  }
     }
     
     
